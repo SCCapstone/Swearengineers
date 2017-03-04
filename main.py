166d208eb0a78e3eb90339f91360f80b5227bec4
jeSager <jamessager@hotmail.com>
2017-03-04 09:44:21 -0500

    update myquizzes

--- main.py
+++ main.py
@@ -610,7 +610,7 @@ class deleteQuizHandler(BaseHandler):
           problem.key.delete()
       quiz_key.delete()
       time.sleep(0.1)
-      self.redirect("/")
+      self.redirect("inMyQuizzes.html")
 
 class editProblemHanlder(BaseHandler):
   @user_required
