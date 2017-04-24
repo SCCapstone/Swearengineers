359b77977b8ea4bee2b2be0656623f7399b54fb2
jeSager <jamessager@hotmail.com>
2017-04-23 20:31:02 -0400

    u

--- mail.py
+++ mail.py
@@ -1,46 +1,69 @@
 #!/usr/bin/env python
 from google.appengine.api import mail
 
-#    mail.send_mail(
-#      sender = 'MathQuizzes<MathQuizzes@math-quizzes-jesager.appspotmail.com>',
-#      to = 'James <jamessager@hotmail.com>',
-#      subject = "Testing",
-#      body = """
-#      <h1>Hi!</h1>
-#      Test test test.
-#      """+'<h3>another</h3>'
-#    )
+WEBSENDER='MathQuizzes<MathQuizzes@math-quizzes-jesager.appspotmail.com>',
 
 def sendit():
-  message = mail.EmailMessage(
-    sender='MathQuizzes<MathQuizzes@math-quizzes-jesager.appspotmail.com>',
-    subject="MSG TEST")
-  message.to = "James Sager <jamessager@hotmail.com>"
+  course_name='Math 101'
+  quiz_name='Quiz 14'
+  quiz_description='Subtraction'
+  if quiz_description:
+    quiz_name=quiz_name + ':  ' + quiz_description
+  quiz_url='http://www.google.com'
+  instructor='Smith'
+  first='James'
+  last='Sager'
+  email='jamessager@hotmail.com'
+  address=first + ' ' + last + '<' + email + '>'
+
+  subject='New Quiz from Instructor' + instructor
+
+  message = mail.EmailMessage(WEBSENDER, subject)
+  message.to = address
 
   message.body = """
-  Dear Albert:
+    Hi, """ + first + '!'+ """
 
-  Your example.com account has been approved.  You can now visit
-  http://www.example.com/ and sign in using your Google Account to
-  access new features.
+    Instructor """ + instructor + """ has assigned you """ + quiz_name + """
+    through your MathQuizzes """ + course_name + """ Course.
 
-  Please let us know if you have any questions.
+    Paste the linke below into your browser to start the quiz:
 
-  The example.com Team
+    """+ quiz_url +"""
+
+    Best of luck from the MathQuizzes Team!
   """
 
   message.html = """
-  <html><head></head><body>
-  <h1>Dear Albert:<h1>
-
-  Your example.com account has been approved.  You can now visit
-  http://www.example.com/ and sign in using your Google Account to
-  access new features.
-
-  <b>Please</b> let us know if you have any questions.
-
-  The example.com Team
-  </body></html>
+    <html>
+    <body style="margin:0;padding:0;
+      font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;">
+      <div
+        style="background:#222;padding:1%;
+        border-bottom: 10px solid #5bc0de">
+        <h2 style="font-weight:normal; color:#FFF; padding-left:2%; margin:2px 0;">
+          <span style="color:#5bc0de">Math</span>Quizzes
+        </h2>
+      </div>
+      <div style="padding: 0 3%; border-bottom:1px solid #CCC">
+        <h2 style="font-weight:normal">Hi, """+ first +"""!</h2>
+        <p>Instructor """+ instructor +""" just assigned you """+ quiz_name +"""
+           through your MathQuizzes """ + course_name + """ Course.</p>
+        <p>Click the link below to start your quiz:</p>
+        <h2 style="color:#3378b7;padding-left:10%;font-weight:normal">
+          <a href='""" + quiz_url + """' title="Take the Quiz!"
+            style="color:#3378b7;">""" + quiz_name + """</a></h2>
+        <br>
+        <h3 style="font-weight:normal">Best of luck from the MathQuizzes Team!</h3>
+        <br>
+        <br>
+      </div>
+    </body>
+    </html>
   """
 
   message.send()
+
+
+
+
--- main.py
+++ main.py
@@ -257,16 +257,6 @@ class MainHandler(BaseHandler, webapp2.RequestHandler):
   @user_required
   def get(self):
     sendit()
-#    mail.send_mail(
-#      sender = 'MathQuizzes<MathQuizzes@math-quizzes-jesager.appspotmail.com>',
-#      to = 'James <jamessager@hotmail.com>',
-#      subject = "Testing",
-#      body = """
-#      <h1>Hi!</h1>
-#      Test test test.
-#      """+'<h3>another</h3>'
-#    )
-
     if self.user.isTeacher:
       if not hasattr(self.user,'selectedCourseKey'):
         self.render_template('instructor/inMyCourses.html', {'newuser': True})
